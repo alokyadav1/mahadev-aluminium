96ac4aecc634ef9f3fe1d7f338d6fb84926b1368
Alok Yadav <alok.yadav8879@gmail.com>
2023-02-10 13:06:29 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#splash-screen").removeClass("show");
+    $("#splash-screen").addClass("hide");
+
+    $("#main-content").removeClass("hide");
+    $("#main-content").addClass("show");
+});
